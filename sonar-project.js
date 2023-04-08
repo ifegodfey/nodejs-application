4524a6b7dfc775c105d242b85f45d43450e51cc3
ifegodfey <125557527+ifegodfey@users.noreply.github.com>
2023-04-08 02:40:02 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '3e9ee4f64a40359b3f9c904bc34392412e2c9d80',
+	    'sonar.login': '21a4a9d3ffdcb93709d59dd2eb1d5dd45f0c9d15',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
